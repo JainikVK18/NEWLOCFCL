ed3d2fd7cf91f3fbb63530b75afb2713544f33f1
JainikVK18 <Jainikpatel2007@gmail.com>
2025-06-15 14:27:50 -0400

    Create jainik.js

---
+++ jainik.js
@@ -0,0 +1,16 @@
+// streams.js
+
+const STREAMS = {
+    mpd1: {
+        url: "https://a204aivottepl-a.akamaihd.net/sin-nitro/live/clients/dash/enc/fdb3pubmek/out/v1/aefca6420f944a9482e117f315de535f/cenc.mpd",
+        keys: {
+            "7e9239c1982d984a002df3ed049d0756": "1b8a17598129a3618535c8fb05f103fe"
+        }
+    },
+    mpd2: {
+        url: "https://example.com/another-stream/manifest.mpd",
+        keys: {
+            "deadbeefdeadbeefdeadbeefdeadbeef": "1234567890abcdef1234567890abcdef"
+        }
+    },
+};
