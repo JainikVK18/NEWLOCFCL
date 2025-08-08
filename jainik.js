fa0f1c15e22f3e072421cbad8315756b5776f9cb
JainikVK18 <Jainikpatel2007@gmail.com>
2025-08-08 10:42:18 -0400

    Update jainik.js

--- jainik.js
+++ jainik.js
@@ -1,10 +1,10 @@
 // streams.js
 
 const STREAMS = {
-    Gt20: {
-        url: "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash-sd/enc/6fdpqu1mii/out/v1/325b5647f46a402a80ae6bdc0300ae64/cenc-sd.mpd",
+    test: {
+        url: "https://live-d-01-icc-we.akamaized.net/variant/v1blackout/vcg-01-d/DASH_DASH/Live/channel(vcg-01-ch-hd-04)/hdntl=exp=1754665900~acl=%2fvariant%2fv1blackout%2fvcg-01-d%2f*~data=hdntl~hmac=2f193a1739d46504f43208140ca98283f40ef00e90b2c07a3d7710021f860e33/manifest.mpd",
         keys: {
-            "9e98aa553a5f68eb6fd4f54f7d47db92": "c85e86b214745af6a5ecd342cd0688c7"
+            "ae7076b647133606b704d33e4cef5134": "810251515584cf71216d9a2d1ebf04bb"
         }
     },
     mpd2: {
